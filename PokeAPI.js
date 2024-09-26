099b6467f189ab6d82cd32dd4d3b1d13faa5388f
JSander72 <jamesearlsanders2@outlook.com>
2024-09-26 04:44:31 -0400

    user_input.toLowerCase

--- PokeAPI.js
+++ PokeAPI.js
@@ -4,27 +4,31 @@ async function fetchPokemonData(pokemonName) {
     if (response.ok) {
         return await response.json();
     } else {
-        throw new Error('Error fetching data');
+        throw new Error('Error fetching data', alert("Pokemon not found"));
     }
 }
 
 document.getElementById("pokemon-form").addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const pokemonName = document.getElementById("search-pokemon").value;
-
-    try {
-        const data = await fetchPokemonData(pokemonName);
-        const pokeInfoElement = document.getElementById("pokemon-info");
-        pokeInfoElement.innerHTML = `
-            <h2>${data.name}</h2>
-            <img src="${data.sprites.front_default}" />
-            <h3>${data.abilities[0].ability.name}</h3>
-            <p>Height: ${data.height}</p>
-            <p>Weight: ${data.weight}</p>
-        `;
-        console.log(data);
-    } catch (error) {
-        console.error('Error fetching data:', error);
+    const pokemonNameInput = document.getElementById("search-pokemon");
+    if (pokemonNameInput) {
+        const pokemonName = pokemonNameInput.value.toLowerCase();
+        try {
+            const data = await fetchPokemonData(pokemonName);
+            const pokeInfoElement = document.getElementById("pokemon-info");
+            pokeInfoElement.innerHTML = `
+                <h2>${data.name}</h2>
+                <img src="${data.sprites.front_default}" />
+                <h3>${data.abilities[0].ability.name}</h3>
+                <p>Height: ${data.height}</p>
+                <p>Weight: ${data.weight}</p>
+            `;
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    } else {
+        console.error('Input element not found');
     }
 });
